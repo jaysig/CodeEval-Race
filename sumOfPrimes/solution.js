42ecdaecaac7c6cf9dec3fe912d1b7ad2ef8aa67
Jaysig <jasontsigmon@gmail.com>
2015-10-31 22:58:31 -0400

    Two problems completed

--- sumOfPrimes/solution.js
+++ sumOfPrimes/solution.js
@@ -8,7 +8,15 @@ fs.readFileSync(filename).toString().split('\n').forEach(function (line) {
 });
 
 function solution(line){
-  // Put your solution code here
-  // Each line represents 1 test case provided by CodeEval
-  return line;
+  var count = 0;
+  for (var i=3; i<1000;i++){
+    add = true;
+    for(var x=2;x<i;x++){
+      if(i % x === 0){
+        add = false;
+      }
+    }
+    count+=i;
+  }
+  return count;
 }
